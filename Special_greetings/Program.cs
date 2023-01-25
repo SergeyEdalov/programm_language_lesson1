5391e2495686488bb94f16786b335f4d98cb21b9
Sergey Edalov <edalov94@gmail.com>
2023-01-25 14:10:09 +0300

    add special greetings for masha

---
+++ Special_greetings/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Wow, it`s MASHA!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
